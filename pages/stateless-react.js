dc2b2edb98e5abb35a85f3b7b1bcb5411ad52448
Andy Carson <andrew.carson@silverpond.com.au>
2016-10-31 11:02:35 +1100

    initial commit

---
+++ pages/stateless-react.js
@@ -0,0 +1,19 @@
+import React from 'react'
+import Helmet from 'react-helmet'
+import { config } from 'config'
+
+
+// Component
+const Stateless = () => {
+  return (
+    <div
+      className="kittens"
+    >
+      I'm still stateless
+      <img src="/images/kitten.jpg" />
+    </div>
+  )
+}
+
+
+export default Stateless
